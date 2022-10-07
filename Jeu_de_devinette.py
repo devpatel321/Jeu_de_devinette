186352d300b75ac3be7b66a77c0f1afae0ca1177
pateld <pateld@educ.college-montreal>
2022-10-07 16:10:00 -0400

    tp2

--- Jeu_de_devinette.py
+++ Jeu_de_devinette.py
@@ -1,25 +1,48 @@
 #Créé par Dev Patel
 #Créé le 29 septembre 2022
-#Créer un jeu de devinette dans lequel l'usager doit trouver le nombre aléatoire (entre 1 et 100) choisi par l'ordinateur
-
-import random
-
-chaine = print("J'ai choisi un nombre entre 0 et 100. Ton objectif est de le deviner")
-essai = None
-nombre_mystere = random.randint(1,100)
-
-count = 1
-if essai != nombre_mystere:
-
-    essai = input("Entrez votre essaie:")
+#Créer un jeu de devinette dans lequel l'usager doit trouver le nombre aléatoire (entre 1 et 10) choisi par l'ordinateur
 
 
-    if essai == nombre_mystere:
-        print("Bravo!")
+from random import *
 
-        print("Ça t'as pris", count, "essaies")
+chaine = print("J'ai choisi un nombre entre 1 et 10. Ton objectif est de le deviner")
 
-    else:
-        print("Réessayer")
-        count += 1
 
+def jeu():
+
+    nb_essais = 0
+
+
+    play_code = True
+
+    while play_code:
+        nombre_mystere = randint(1, 10)
+
+        not_found = True
+        while not_found:
+
+            essai = int(input("Entrez votre essai:"))
+
+            if essai < nombre_mystere:
+                print("Le nombre mystère est plus gros")
+                nb_essais += 1
+
+            elif essai > nombre_mystere:
+                print("Le nombre mystère est plus petit")
+                nb_essais += 1
+
+            elif essai == nombre_mystere:
+                print(f"Bravo! Vous avez devinez le numéro en {nb_essais} essaies.")
+                restart = input("Si vous voulez rejouer, écrivez (oui). Sinon, écrivez (non)")
+
+
+                if restart == "oui":
+                    play_code = True
+
+                elif restart == "non":
+                    play_code = False
+
+                else:
+                    play_code = False
+
+jeu()
