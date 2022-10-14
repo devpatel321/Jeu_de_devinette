36a49aea0f27c2026dedee7ac5d8e6ce95bd4a75
pateld <pateld@educ.college-montreal>
2022-10-14 08:15:26 -0400

    jeu devinette

--- Jeu_de_devinette.py
+++ Jeu_de_devinette.py
@@ -1,48 +1,48 @@
-#Créé par Dev Patel
-#Créé le 29 septembre 2022
-#Créer un jeu de devinette dans lequel l'usager doit trouver le nombre aléatoire (entre 1 et 10) choisi par l'ordinateur
+"""
+Créé par Dev Patel
+Créé le 29 septembre 2022
+Créer un jeu de devinette dans lequel l'usager les limites pour le nombre aléatoire
+"""
 
-
-from random import *
-
-chaine = print("J'ai choisi un nombre entre 1 et 10. Ton objectif est de le deviner")
+from random import randint
 
 
 def jeu():
+    """
+    jeu() correspond à la fonction qui exécute le jeu et permet au joueur d'installer des limites pour le nombre aléatoire
+    """
 
-    nb_essais = 0
+    jeu_commence = True
 
+    while jeu_commence:
+        limite_1 = int(input("Entrez la limite pour le plus petit nombre:"))
+        limite_2 = int(input("Entrez la limite pour le plus gros nombre:"))
 
-    play_code = True
-
-    while play_code:
-        nombre_mystere = randint(1, 10)
+        nombre_mystere = randint(limite_1, limite_2)
+        nb_essais = 1
 
         not_found = True
         while not_found:
-
             essai = int(input("Entrez votre essai:"))
-
-            if essai < nombre_mystere:
+            if essai < limite_1 or essai > limite_2:
+                print("Votre nombre dépasse les limites")
+            elif essai < nombre_mystere:
                 print("Le nombre mystère est plus gros")
                 nb_essais += 1
-
             elif essai > nombre_mystere:
                 print("Le nombre mystère est plus petit")
                 nb_essais += 1
-
-            elif essai == nombre_mystere:
+            else:
                 print(f"Bravo! Vous avez devinez le numéro en {nb_essais} essaies.")
-                restart = input("Si vous voulez rejouer, écrivez (oui). Sinon, écrivez (non)")
+                not_found = False
 
+                restart = input("Si vous voulez rejouer, tapez (o). Sinon, tapez (n)")
 
-                if restart == "oui":
-                    play_code = True
+                if restart == "o":
+                    jeu_commence = True
+                elif restart == "n":
+                    print("Au revoir!")
+                    jeu_commence = False
 
-                elif restart == "non":
-                    play_code = False
-
-                else:
-                    play_code = False
 
 jeu()
